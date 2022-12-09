ed810f28b557f209b72a451d882fe60c5d54b539
Lucas Johnson <contact@lucasjohnson.co.nz>
2022-12-09 13:30:41 +0100

    Compile SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,43 @@
-var gulp = require("gulp");
-var inky = require("inky");
+const browserSync = require("browser-sync").create();
+const gulp = require("gulp");
+const inky = require("inky");
+const inlineCss = require("gulp-inline-css");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const replace = require("gulp-replace");
 
-gulp.task("parse", function() {
+const baseDir = "./dist";
+
+gulp.task("scss", function() {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./src/css"));
+});
+
+gulp.task("html", function() {
   return gulp
     .src("src/templates/**/*.html")
+    .pipe(replace(new RegExp("/sass/(.+).scss", "ig"), "/css/$1.css"))
     .pipe(inky())
+    .pipe(inlineCss())
+    .pipe(rename({ dirname: "" }))
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task("browserSync", function() {
+  return browserSync.init({
+    server: { baseDir: baseDir }
+  });
+});
+
+gulp.task("reloadBrowserSync", function() {
+  return browserSync.reload();
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./src/scss/**/*.scss", gulp.series("scss"));
+  gulp.watch("./src/templates/**/*.html", gulp.series("html"));
+});
+
+gulp.task("default", gulp.parallel("watch", "browserSync"));
